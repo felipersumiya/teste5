8973283c247e58839e59ddee3e29280f99e2ad99
felipersumiya <felipersumiya@gmail.com>
2021-06-25 18:26:57 -0300

    teste

---
+++ src/main/java/com/example/demo/Teste5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste5Application.class, args);
+	}
+
+}
